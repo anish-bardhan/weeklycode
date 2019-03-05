d7d9f2aecd762ed3486da6cdf21b6b1c889f37ca
Anish Bardhan <2020abardhan@dwight.edu>
2019-03-04 20:00:30 -0500

    happy

--- caesar.py
+++ caesar.py
@@ -1,11 +1,14 @@
 def caesar(string, shift):
-    #create
+    #create alphabet arrays
     alpha = list("abcdefghijklmnopqrstuvwxyz")
     ALPHA = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    #create blank hashes
     dictionary1 = {}
     dictionary2 = {}
     dictionary3 = {}
+    #set count variable
     count = 1
+    #fill hashes
     for x in range(len(alpha)):
         dictionary1[x+1] = alpha[x]
     for y in alpha:
@@ -15,20 +18,30 @@ def caesar(string, shift):
         dictionary3[y] = count
         count += 1
 
+    #create array for list
     word = list(string)
+    #create new word array
     new_word = []
-
+    #loop through letters in word
     for letter in word:
+        #check if letter is in alphabet
         if letter in alpha:
+            #make a position using number
             position = dictionary2[letter]
+            #add position and shift
             position += shift
+            #check if number is too large
             if position > 26:
+                #put number in right place
                 position -= 26
+                #find new letter
                 new_letter = dictionary1[position]
+                #add letter to word
                 new_word.append(new_letter)
             else:
                 new_letter = dictionary1[position]
                 new_word.append(new_letter)
+        #check if letter is upper case
         elif letter in ALPHA:
             position = dictionary3[letter]
             position += shift
@@ -42,13 +55,14 @@ def caesar(string, shift):
         else:
             new_word.append(letter)
 
+    #join word
     print(''.join(new_word))
 
+#call function
 caesar("happy am i!", 1)
 
 def decrypt(string, shift):
     alpha = list("abcdefghijklmnopqrstuvwxyz")
-    numbers = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26)
     dictionary1 = {}
     dictionary2 = {}
     count = 1
